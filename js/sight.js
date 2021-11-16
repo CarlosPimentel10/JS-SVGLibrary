15d49245ec53bf9799b21b26f3111c0af694f03e
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2021-11-16 22:18:01 +0100

    correct order of parameters

--- js/sight.js
+++ js/sight.js
@@ -5,7 +5,7 @@ class SVGElement {
     constructor(type){
         this.type = type;
         this.namespace = 'http://www.w3.org/2000/svg';
-        this.node = document.createElementNS(this.type, this.namespace);
+        this.node = document.createElementNS(this.namespace, this.type);
         return this;
     }
     attr(attrs){
