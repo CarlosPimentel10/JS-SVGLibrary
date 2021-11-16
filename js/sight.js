7d5f80d493b229a1834853ffca7e3cd87a4f1fe2
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2021-11-16 21:58:09 +0100

    added node

--- js/sight.js
+++ js/sight.js
@@ -2,8 +2,10 @@
 class SVGElement {
     
 
-    constructor(type, namespace){
+    constructor(type, namespace, node){
         this.type = type;
-        this.namespace = 'http://www.w3.org/2000/svg';;
+        this.namespace = 'http://www.w3.org/2000/svg';
+
+        this.node = document.createElementNS();
     }
 }
